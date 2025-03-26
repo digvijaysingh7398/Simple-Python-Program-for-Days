cf0018f7976c26fc72dce138e416e911ff8af7c8
digvijaysingh7398 <digvijaysingh7398@gmail.com>
2025-03-26 12:40:04 +0530

    Add files via upload

---
+++ P.py
@@ -0,0 +1,19 @@
+t='Reshama'
+print(t)
+print(type(t))
+b="I love 'Python' coding"
+print(b)
+c='I love "Python" coding'
+print(c)
+
+d="""New "Shakti" 'College' OF IT"""
+print(d)
+
+
+a="        Rishu    "
+print(a)
+print(len(a))
+
+print(a.strip())
+print(a.rstrip())
+print(a.lstrip())
